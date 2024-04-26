974a758223504e22cbd08e3eff45de258562ef50
pysyve <94847523+pysyve0730@users.noreply.github.com>
2024-04-26 15:17:01 +0800

    Update 70. Climbing Stairs.cpp
    Recursive cause time exceed, so use iterative with STL

--- 70. Climbing Stairs.cpp
+++ 70. Climbing Stairs.cpp
@@ -1,3 +1,5 @@
+
+//recursive
 class Solution {
 public:
     int climbStairs(int n) {
@@ -8,6 +10,25 @@ public:
         }else{
             return climbStairs(n-1)+ climbStairs(n-2);
         }
+    }
+};
+
+//iterative
+class Solution {
+public:
+    int climbStairs(int n) {
+       if (n <= 2) {
+            return n;
+        }
+        vector<int> v(n);
+        v[0] = 1;
+        v[1] = 2;
+        for (int i = 2; i < n; ++i) {
+            v[i] = v[i - 1] + v[i - 2];
+        }
+        int result = v[n - 1];
+        return result;
+
 
     }
 };
